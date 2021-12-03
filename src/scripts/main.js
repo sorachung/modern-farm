38cd1b9990a360984f765a0a0eda20ad98dff5b9
sorachung <sorachung.src@gmail.com>
2021-12-03 09:31:04 -0600

    added seeds and export f's

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,22 @@
+import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
+
+
 console.log("Welcome to the main module")
 
+const yearlyPlan = createPlan();
+
+console.log(yearlyPlan);
+
+const asparagusSeed = createAsparagus()
+const cornSeed = createCorn();
+const potatoSeed = createPotato();
+const soybeanSeed = createSoybean();
+const sunflowerSeed = createSunflower();
+const wheatSeed = createWheat();
+console.log(asparagusSeed, cornSeed, potatoSeed,soybeanSeed,sunflowerSeed,wheatSeed)
---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,7 @@
+export const createCorn = () => {
+  return { 
+    type: "Corn", 
+    height: 180, 
+    output: 6 
+  };
+};

a0ae35a3deb6bf7890e9244e3fa4568d392ab20a
sorachung <sorachung.src@gmail.com>
2021-12-03 14:17:18 -0600

    fixed spelling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ import { createPotato } from "./seeds/potato.js"
 import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createWheat } from "./seeds/wheat.js"
-import { addPlant, usePlants } from "./fields.js"
+import { addPlant, usePlants } from "./field.js"
 import { plantSeeds } from "./tractor.js"
 
 console.log("Welcome to the main module")
--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -4,7 +4,7 @@ import { createPotato } from "./seeds/potato.js"
 import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createWheat } from "./seeds/wheat.js"
-import { addPlant } from "./fields.js";
+import { addPlant } from "./field.js";
 
 export const plantSeeds = (plantingPlan) => {
   plantingPlan.forEach((row) => row.forEach((plant) => {
